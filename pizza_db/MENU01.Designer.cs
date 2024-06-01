00d87407a33d7c6d9cbff3782d20cdfe3c9616bb
LENOVO <LENOVO@LAPTOP-8P9FK2ED>
2024-06-02 01:53:52 +0700

    --

---
+++ pizza_db/MENU01.Designer.cs
@@ -0,0 +1,489 @@
+﻿namespace pizza_db
+{
+    partial class MENU01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            textBox10 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            pictureBox3 = new PictureBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            pictureBox2 = new PictureBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            label_username = new Label();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            pictureBox4 = new PictureBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
+            pictureBox5 = new PictureBox();
+            textBox17 = new TextBox();
+            textBox18 = new TextBox();
+            textBox19 = new TextBox();
+            pictureBox6 = new PictureBox();
+            button4 = new Button();
+            textBox20 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(21, 667);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(109, 87);
+            button3.TabIndex = 36;
+            button3.Text = "MyOrder";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(21, 761);
+            textBox10.Margin = new Padding(3, 4, 3, 4);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(139, 27);
+            textBox10.TabIndex = 35;
+            textBox10.Text = "Total price ";
+            textBox10.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(785, 717);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(229, 87);
+            button2.TabIndex = 34;
+            button2.Text = "Continue to PAYMENT";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1046, 31);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 87);
+            button1.TabIndex = 33;
+            button1.Text = "Logout";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(21, 575);
+            textBox7.Margin = new Padding(3, 4, 3, 4);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(404, 69);
+            textBox7.TabIndex = 32;
+            textBox7.Text = "\r\ndescription";
+            textBox7.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(311, 653);
+            textBox8.Margin = new Padding(3, 4, 3, 4);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(114, 27);
+            textBox8.TabIndex = 31;
+            textBox8.Text = "PRICE";
+            textBox8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(21, 536);
+            textBox9.Margin = new Padding(3, 4, 3, 4);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(404, 27);
+            textBox9.TabIndex = 30;
+            textBox9.Text = "PIZZA01";
+            textBox9.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(432, 515);
+            pictureBox3.Margin = new Padding(3, 4, 3, 4);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(160, 169);
+            pictureBox3.TabIndex = 29;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(21, 397);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(404, 69);
+            textBox4.TabIndex = 28;
+            textBox4.Text = "\r\ndescription";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(311, 476);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(114, 27);
+            textBox5.TabIndex = 27;
+            textBox5.Text = "PRICE";
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(21, 359);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(404, 27);
+            textBox6.TabIndex = 26;
+            textBox6.Text = "PIZZA01";
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(432, 337);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(160, 169);
+            pictureBox2.TabIndex = 25;
+            pictureBox2.TabStop = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(21, 220);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(404, 69);
+            textBox3.TabIndex = 24;
+            textBox3.Text = "\r\ndescription";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(311, 299);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(114, 27);
+            textBox2.TabIndex = 23;
+            textBox2.Text = "PRICE";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 181);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(404, 27);
+            textBox1.TabIndex = 22;
+            textBox1.Text = "PIZZA01";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(432, 160);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(160, 169);
+            pictureBox1.TabIndex = 21;
+            pictureBox1.TabStop = false;
+            // 
+            // label_username
+            // 
+            label_username.AutoSize = true;
+            label_username.Location = new Point(56, 31);
+            label_username.Name = "label_username";
+            label_username.Size = new Size(75, 20);
+            label_username.TabIndex = 20;
+            label_username.Text = "Username";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(609, 220);
+            textBox11.Margin = new Padding(3, 4, 3, 4);
+            textBox11.Multiline = true;
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(404, 69);
+            textBox11.TabIndex = 40;
+            textBox11.Text = "\r\ndescription";
+            textBox11.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(899, 299);
+            textBox12.Margin = new Padding(3, 4, 3, 4);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(114, 27);
+            textBox12.TabIndex = 39;
+            textBox12.Text = "PRICE";
+            textBox12.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(609, 181);
+            textBox13.Margin = new Padding(3, 4, 3, 4);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(404, 27);
+            textBox13.TabIndex = 38;
+            textBox13.Text = "PIZZA01";
+            textBox13.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(1021, 160);
+            pictureBox4.Margin = new Padding(3, 4, 3, 4);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(160, 169);
+            pictureBox4.TabIndex = 37;
+            pictureBox4.TabStop = false;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(609, 397);
+            textBox14.Margin = new Padding(3, 4, 3, 4);
+            textBox14.Multiline = true;
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(404, 69);
+            textBox14.TabIndex = 44;
+            textBox14.Text = "\r\ndescription";
+            textBox14.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(899, 476);
+            textBox15.Margin = new Padding(3, 4, 3, 4);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(114, 27);
+            textBox15.TabIndex = 43;
+            textBox15.Text = "PRICE";
+            textBox15.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(609, 359);
+            textBox16.Margin = new Padding(3, 4, 3, 4);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(404, 27);
+            textBox16.TabIndex = 42;
+            textBox16.Text = "PIZZA01";
+            textBox16.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(1021, 337);
+            pictureBox5.Margin = new Padding(3, 4, 3, 4);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(160, 169);
+            pictureBox5.TabIndex = 41;
+            pictureBox5.TabStop = false;
+            // 
+            // textBox17
+            // 
+            textBox17.Location = new Point(609, 575);
+            textBox17.Margin = new Padding(3, 4, 3, 4);
+            textBox17.Multiline = true;
+            textBox17.Name = "textBox17";
+            textBox17.Size = new Size(404, 69);
+            textBox17.TabIndex = 48;
+            textBox17.Text = "\r\ndescription";
+            textBox17.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox18
+            // 
+            textBox18.Location = new Point(899, 653);
+            textBox18.Margin = new Padding(3, 4, 3, 4);
+            textBox18.Name = "textBox18";
+            textBox18.Size = new Size(114, 27);
+            textBox18.TabIndex = 47;
+            textBox18.Text = "PRICE";
+            textBox18.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox19
+            // 
+            textBox19.Location = new Point(609, 536);
+            textBox19.Margin = new Padding(3, 4, 3, 4);
+            textBox19.Name = "textBox19";
+            textBox19.Size = new Size(404, 27);
+            textBox19.TabIndex = 46;
+            textBox19.Text = "PIZZA01";
+            textBox19.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(1021, 515);
+            pictureBox6.Margin = new Padding(3, 4, 3, 4);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(160, 169);
+            pictureBox6.TabIndex = 45;
+            pictureBox6.TabStop = false;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1046, 712);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(129, 63);
+            button4.TabIndex = 49;
+            button4.Text = "NEXT";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox20
+            // 
+            textBox20.Location = new Point(1146, 783);
+            textBox20.Margin = new Padding(3, 4, 3, 4);
+            textBox20.Name = "textBox20";
+            textBox20.Size = new Size(28, 27);
+            textBox20.TabIndex = 81;
+            textBox20.Text = "1/2";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(21, 68);
+            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(163, 59);
+            button5.TabIndex = 82;
+            button5.Text = "Edit profile";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(211, 68);
+            button6.Margin = new Padding(3, 4, 3, 4);
+            button6.Name = "button6";
+            button6.Size = new Size(163, 59);
+            button6.TabIndex = 83;
+            button6.Text = "Edit payment";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // MENU01
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(1189, 581);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(textBox20);
+            Controls.Add(button4);
+            Controls.Add(textBox17);
+            Controls.Add(textBox18);
+            Controls.Add(textBox19);
+            Controls.Add(pictureBox6);
+            Controls.Add(textBox14);
+            Controls.Add(textBox15);
+            Controls.Add(textBox16);
+            Controls.Add(pictureBox5);
+            Controls.Add(textBox11);
+            Controls.Add(textBox12);
+            Controls.Add(textBox13);
+            Controls.Add(pictureBox4);
+            Controls.Add(button3);
+            Controls.Add(textBox10);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox8);
+            Controls.Add(textBox9);
+            Controls.Add(pictureBox3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox6);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label_username);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "MENU01";
+            Text = "MENU";
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button3;
+        private TextBox textBox10;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private PictureBox pictureBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private PictureBox pictureBox2;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private Label label_username;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private PictureBox pictureBox4;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox16;
+        private PictureBox pictureBox5;
+        private TextBox textBox17;
+        private TextBox textBox18;
+        private TextBox textBox19;
+        private PictureBox pictureBox6;
+        private Button button4;
+        private TextBox textBox20;
+        private Button button5;
+        private Button button6;
+    }
+}
